1874bdc03c099359ce7a6bc12b2a770a318bec61
Henrique Domiciano <henriquedomiciano@yahoo.com>
2026-05-13 00:14:25 -0300

    [FEAT] Add initial security feature

--- main/main.c
+++ main/main.c
@@ -4,7 +4,10 @@
 #include "host/ble_gap.h"
 #include "host/ble_gatt.h"
 #include "host/ble_hs.h"
+#include "host/ble_sm.h"
 #include "include.h"
+#include "nimble/nimble_port.h"
+#include "services/gap/ble_svc_gap.h"
 #include "soc/gpio_num.h"
 #include <stdbool.h>
 #include <stdint.h>
@@ -90,16 +93,8 @@ static void ble_spp_server_advertise(void) {
 
   memset(&fields, 0, sizeof fields);
 
-  /* Advertise two flags:
-   *     o Discoverability in forthcoming advertisement (general)
-   *     o BLE-only (BR/EDR unsupported).
-   */
-  fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP;
+  fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP | BLE_HS;
 
-  /* Indicate that the TX power level field should be included; have the
-   * stack fill this value automatically.  This is done by assigning the
-   * special value BLE_HS_ADV_TX_PWR_LVL_AUTO.
-   */
   fields.tx_pwr_lvl_is_present = 1;
   fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
 
@@ -162,9 +157,23 @@ static int ble_spp_server_gap_event(struct ble_gap_event *event, void *arg) {
                                         .supervision_timeout = 200};
 
     ble_gap_update_params(active_conn, &params);
-    return 0; 
 #endif
+#ifdef CONFIG_BLE_SECURITY_STATIC_KEY
+    ble_gap_security_initiate(event->connect.conn_handle);
+#endif
+    return 0;
+#ifdef CONFIG_BLE_SECURITY_STATIC_KEY
+  case BLE_GAP_EVENT_PASSKEY_ACTION:
+    if (event->passkey.params.action == BLE_SM_IOACT_DISP) {
+      struct ble_sm_io pkey = {0};
+      pkey.action = event->passkey.params.action;
 
+      pkey.passkey = 777888;
+
+      ble_sm_inject_io(event->passkey.conn_handle, &pkey);
+    }
+    return 0;
+#endif
   case BLE_GAP_EVENT_PHY_UPDATE_COMPLETE:
     ESP_LOGI("BLE", "TX PHY: %d RX PHY: %d", event->phy_updated.tx_phy,
              event->phy_updated.rx_phy);
@@ -568,34 +577,59 @@ void ble_spp_uart_init(UartConnection *uart_connection_attributes) {
   uart_driver_install(uart_connection_attributes->Uart_port, 16384, 16384, 10,
                       &uart_connection_attributes->Uart_queue_handle, 0);
   MODLOG_DFLT(INFO, "Queue addr: %p\n",
-              uart_connection_attributes->Uart_queue_handle); 
+              uart_connection_attributes->Uart_queue_handle);
   uart_param_config(uart_connection_attributes->Uart_port, &uart_config);
   uart_set_pin(uart_connection_attributes->Uart_port,
                uart_connection_attributes->tx, uart_connection_attributes->rx,
                UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-  
+
   gpio_pullup_en(uart_connection_attributes->rx);
   xTaskCreate(ble_server_uart_task, "uTask", 4096,
               (void *)uart_connection_attributes, 5, NULL);
 }
 
+esp_power_level_t
+get_esp32_power_value_based_on_int(int8_t power_configuration) {
 
-esp_power_level_t get_esp32_power_value_based_on_int(int8_t power_configuration) {
-    
-    esp_power_level_t power;
+  esp_power_level_t power;
 
-    switch (power_configuration) {
-        case 9:  power = ESP_PWR_LVL_P12; break;
-        case 7:  power = ESP_PWR_LVL_P9; break;
-        case 5:  power = ESP_PWR_LVL_P6; break;
-        case 3:  power = ESP_PWR_LVL_P3; break;
-        case 0:  power = ESP_PWR_LVL_N0; break;
-        case -2: power = ESP_PWR_LVL_N3; break;
-        case -6: power = ESP_PWR_LVL_N6; break;
-        case -12:power = ESP_PWR_LVL_N12; break;
-        default: power = ESP_PWR_LVL_P3; break;
-    }
-    return power;
+  switch (power_configuration) {
+  case 9:
+    power = ESP_PWR_LVL_P12;
+    break;
+  case 7:
+    power = ESP_PWR_LVL_P9;
+    break;
+  case 5:
+    power = ESP_PWR_LVL_P6;
+    break;
+  case 3:
+    power = ESP_PWR_LVL_P3;
+    break;
+  case 0:
+    power = ESP_PWR_LVL_N0;
+    break;
+  case -2:
+    power = ESP_PWR_LVL_N3;
+    break;
+  case -6:
+    power = ESP_PWR_LVL_N6;
+    break;
+  case -12:
+    power = ESP_PWR_LVL_N12;
+    break;
+  default:
+    power = ESP_PWR_LVL_P3;
+    break;
+  }
+  return power;
+}
+
+void configure_sm_static_key_init(void) {
+  ble_hs_cfg.sm_bonding = 1;
+  ble_hs_cfg.sm_mitm = 1;
+  ble_hs_cfg.sm_sc = 1;
+  ble_hs_cfg.sm_io_cap = BLE_SM_IO_CAP_DISP_ONLY;
 }
 
 void app_main(void) {
@@ -646,33 +680,24 @@ void app_main(void) {
   ble_hs_cfg.sync_cb = ble_spp_server_on_sync;
   ble_hs_cfg.gatts_register_cb = gatt_svr_register_cb;
   ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
-
+#ifdef CONFIG_BLE_SECURITY_STATIC_KEY
+  configure_sm_static_key_init();
+#else
   ble_hs_cfg.sm_io_cap = BLE_HS_IO_NO_INPUT_OUTPUT;
-#ifdef CONFIG_EXAMPLE_BONDING
-  ble_hs_cfg.sm_bonding = 1;
 #endif
-#ifdef CONFIG_EXAMPLE_MITM
-  ble_hs_cfg.sm_mitm = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_USE_SC
-  ble_hs_cfg.sm_sc = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_BONDING
-  ble_hs_cfg.sm_our_key_dist = 1;
-  ble_hs_cfg.sm_their_key_dist = 1;
-#endif
-  /* Register custom service */
+  
   rc = gatt_svr_init();
   assert(rc == 0);
-  /* Set the default device name. */
   rc = ble_svc_gap_device_name_set(BLE_ADVERTISING_NAME);
   assert(rc == 0);
-  /* XXX Need to have template for store */
   ble_store_config_init();
+  
   nimble_port_freertos_init(ble_spp_server_host_task);
   ble_att_set_preferred_mtu(BLE_PREFERRED_MTU);
-  esp_power_level_t default_power = get_esp32_power_value_based_on_int(CONFIG_BLE_TX_POWER_ADV);
-  esp_power_level_t advertise_power = get_esp32_power_value_based_on_int(CONFIG_BLE_TX_POWER_DEFAULT);
+  esp_power_level_t default_power =
+      get_esp32_power_value_based_on_int(CONFIG_BLE_TX_POWER_ADV);
+  esp_power_level_t advertise_power =
+      get_esp32_power_value_based_on_int(CONFIG_BLE_TX_POWER_DEFAULT);
   esp_ble_tx_power_set(advertise_power, BLE_DEFAULT_POWER);
   esp_ble_tx_power_set(default_power, BLE_ADVERTISING_POWER);
 }
